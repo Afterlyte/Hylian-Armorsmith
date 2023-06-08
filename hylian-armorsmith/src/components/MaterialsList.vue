a5540ac0cf537e9431f7bffa504eac1e8effbf82
schemcharger <ngraham7@ewu.edu>
2023-06-08 14:31:01 -0700

    made material list dynamic

--- hylian-armorsmith/src/components/MaterialsList.vue
+++ hylian-armorsmith/src/components/MaterialsList.vue
@@ -5,9 +5,11 @@
         <div class="text-center">Materials List</div>
       </v-card-title>
       <v-list class="pb-4">
-        <v-row class="justify-center" v-for="m in materials" :key="m.typeName">
-          {{ m.typeName }}: {{ m.count }}
-        </v-row>
+        <div v-for="m in materials" :key="m.typeName">
+          <v-row class="justify-center" v-if="m.count>0">
+            {{ m.typeName }}: {{ m.count }}
+          </v-row>
+        </div>
       </v-list>
     </v-card>
   </div>
@@ -17,10 +19,12 @@
 import { Material, MaterialType } from "@/scripts/material";
 import { ref } from "vue";
 
-const materials = ref<Array<Material>>();
+const materials = ref<Array<Material>>([]);
+  
+for (const type of Object.values(MaterialType)) {
+  const material = new Material(type, 0);
+  materials.value.push(material);
+}
 
-materials.value = [
-  new Material(MaterialType.BokoblinHorn, 3),
-  new Material(MaterialType.ChuchuJelly, 3),
-];
+materials.value[1].count = 1;
 </script>
