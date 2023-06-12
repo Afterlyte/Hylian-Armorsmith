735bb85a5ff7d7c6b98bf350a3059d39700af35a
Neriah Graham <ngraham7@ewu.edu>
2023-06-11 21:43:29 -0700

    Store pattern

--- hylian-armorsmith/src/components/MaterialsList.vue
+++ hylian-armorsmith/src/components/MaterialsList.vue
@@ -5,7 +5,7 @@
         <div class="text-center">Materials List</div>
       </v-card-title>
       <v-list class="pb-4">
-        <div v-for="m in materials" :key="m.typeName">
+        <div v-for="m in store.materialList" :key="m.typeName">
           <v-row class="justify-center" v-if="m.count>0">
             {{ m.typeName }}: {{ m.count }}
           </v-row>
@@ -16,15 +16,5 @@
 </template>
 
 <script setup lang="ts">
-import { Material, MaterialType } from "@/scripts/material";
-import { ref } from "vue";
-
-const materials = ref<Array<Material>>([]);
-  
-for (const type of Object.values(MaterialType)) {
-  const material = new Material(type, 0);
-  materials.value.push(material);
-}
-
-materials.value[1].count = 1;
+import { store } from "@/scripts/store";
 </script>
